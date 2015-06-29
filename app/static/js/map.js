a98f3df17f3d540d4956e74cd33faa1a5eb71d74
Johanna Weinsziehr <johanna.weinsziehr@gmail.com>
2015-06-29 16:28:33 +0200

    update only markers, not entire map

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
-    window.setInterval(function initialize() {
+    var map;
+    var all_scooter = [];
+    var infowindow = new google.maps.InfoWindow();
+    var marker, i;
+
+    function initialize() {
+        map = new google.maps.Map(document.getElementById('map_canvas'), {
+            zoom: 10,
+            center: new google.maps.LatLng(52.711171, 13.67874),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+    }
+
+    window.setInterval(function getScooter() {
         console.log("ajax call...");
         $.ajax({
             url: '/get_scooter',
             type: "GET",
             success: function (data) {
                 console.log("ajax call");
-                var all_scooter = []; // initialize empty list for scooters
+                all_scooter = []; // initialize empty list for scooters
                 $.each(data, function () {
                     console.log("Data:" + data);
                     var scooter = [];
@@ -28,30 +41,25 @@ $(document).ready(function() {
                     all_scooter.push(scooter);
                 });
                 console.log(all_scooter);
-
-                var map = new google.maps.Map(document.getElementById('map_canvas'), {
-                    zoom: 10,
-                    center: new google.maps.LatLng(52.711171, 13.67874),
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                });
-                var infowindow = new google.maps.InfoWindow();
-
-                var marker, i;
-
-                for (i = 0; i < all_scooter.length; i++) {
-                    marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(all_scooter[i][0], all_scooter[i][2]),
-                        map: map
-                    });
-
-                    google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                        return function () {
-                            infowindow.setContent(all_scooter[i][1]);
-                            infowindow.open(map, marker);
-                        }
-                    })(marker, i));
-                }
+                setMarker(all_scooter);
             }
         })
-    }, 5000);
+    }, 1000);
+
+    function setMarker(all_scooter) {
+        for (i = 0; i < all_scooter.length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(all_scooter[i][0], all_scooter[i][2]),
+                map: map
+            });
+
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                return function () {
+                    infowindow.setContent(all_scooter[i][1]);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        }
+    }
+    initialize();
 });
